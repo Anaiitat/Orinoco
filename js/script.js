b3d89fd86a5046e3fc6c7ce011d1ef7552c14d68
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2021-05-21 23:34:46 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const container = document.getElementById("bear");
 
-fetch("http://localhost:3000/api/teddies")
+fetch("${apiUrl}/api/teddies")
     .then(response => response.json())
     .then(teddies => {
         console.log(teddies);
